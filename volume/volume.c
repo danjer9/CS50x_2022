ebec4536e0fed372f6f1673fb2b84f1804974ac3
youtube2midi <uploadeverything1@gmail.com>
2022-05-16 00:33:45 -0400

    25mb limit

---
+++ volume/volume.c
@@ -0,0 +1,50 @@
+// Modifies the volume of an audio file
+#include <cs50.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char * argv[]) {
+  // Check command-line arguments
+  if (argc != 4) {
+    printf("Usage: ./volume input.wav output.wav factor\n");
+    return 1;
+  }
+
+  // Open files and determine scaling factor
+  FILE * input = fopen(argv[1], "r");
+  if (input == NULL) {
+    printf("Could not open file.\n");
+    return 1;
+  }
+
+  FILE * output = fopen(argv[2], "w");
+  if (output == NULL) {
+    printf("Could not open file.\n");
+    return 1;
+  }
+
+  float factor = atof(argv[3]);
+
+  // TODO: Copy header from input file to output file
+  uint8_t headers;
+  for (int i = 0; i < HEADER_SIZE; i++) {
+    fread( & headers, sizeof(uint8_t), 1, input);
+    fwrite( & headers, sizeof(uint8_t), 1, output);
+  }
+
+  // TODO: Read samples from input file and write updated data to output file
+  int16_t buffer;
+  while (fread( & buffer, sizeof(buffer), 1, input)) {
+    buffer *= factor;
+    fwrite( & buffer, sizeof(buffer), 1, output);
+  }
+
+  // Close files
+  fclose(input);
+  fclose(output);
+}
